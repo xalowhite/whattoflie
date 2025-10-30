b7815ea2f285062705031bac404dd7d1fc475e2c
Carter White <cmw070@uark.edu>
2025-10-29 19:41:52 -0500

    chore: add FlyDetailDialog stub to fix build

---
+++ components/FlyDetailDialog.tsx
@@ -0,0 +1,9 @@
+// components/FlyDetailDialog.tsx
+'use client';
+
+export default function FlyDetailDialog(_props: any) {
+  return null; // TODO: replace with real dialog UI
+}
+
+// Optional named export (safe if someone imports it this way)
+export const FlyDetailDialog = (props: any) => <></>;
