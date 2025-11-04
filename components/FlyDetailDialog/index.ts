cf03027225a42a933992a98c13f9451007268791
Carter White <cmw070@uark.edu>
2025-11-03 20:55:16 -0600

    fix(build): replace FlyDetailDialog with single-file client component

--- components/FlyDetailDialog/index.ts
+++
@@ -1,4 +0,0 @@
-export { default } from './FlyDetailDialog'
-
-// Re-export the global type for any legacy `import { FlyDetailData } from '@/components/FlyDetailDialog'`
-export type FlyDetailData = globalThis.FlyDetailData
